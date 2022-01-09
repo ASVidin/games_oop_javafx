2d230288a97aa744bfe977e50e3c9c96e4ea9aad
ASVidin <Andrey.Vidin@gmail.com>
2022-01-09 12:02:34 +0300

    2. Каркас шахматной доски [#792]

---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.Test;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class BishopBlackTest {
+
+    @Test
+    public void whenPositionF8() {
+        Figure bishop = new BishopBlack(Cell.F8);
+        Cell actual = bishop.position();
+        Cell expected = Cell.F8;
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenCopyFromC8ToE6() {
+        Figure bishop = new BishopBlack(Cell.C8).copy(Cell.E6);
+        Cell actual = bishop.position();
+        Cell expected = Cell.E6;
+        assertThat(actual, is(expected));
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void whenIncorrectMove() {
+        Figure bishop = new BishopBlack(Cell.C1);
+        bishop.way(Cell.F5);
+    }
+}
